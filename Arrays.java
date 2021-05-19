477f6493030db5f9c80dd52d137719f5459a82ed
vishwa1294 <vishwanathjatti@gmail.com>
2021-05-19 12:08:12 +0530

    Adds vaish file

---
+++ Arrays.java
@@ -0,0 +1,16 @@
+class Arrays{
+public static void main(String[]args){
+int evenNumbers[]= {6,8,10,12,14,16,18,20};
+System.out.println(evenNumbers[5]);
+System.out.println("size of evenNumbers "+evenNumbers.length);
+int []oddNumbers=new int[7];
+System.out.println("size of oddNumbers "+oddNumbers.length);
+System.out.println(oddNumbers[0]);
+oddNumbers[0]=15;
+for (int i = 0; i <evenNumbers.length; i++){
+System.out.println(evenNumbers[1]);
+}
+
+}
+
+}
